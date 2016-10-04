3ef808a311d409e580ce590e30a8a6a804efa668
Martin Halamíček <martin@halamicek.cz>
2016-10-04 09:28:35 +0200

    snowflake - odbc 2.12.87

--- docker/downloadOdbcDriver.php
+++ docker/downloadOdbcDriver.php
@@ -23,6 +23,6 @@ $client =  new \Aws\S3\S3Client([
 
 $client->getObject([
     'Bucket' => 'keboola-configs',
-    'Key' => 'drivers/snowflake/snowflake_linux_x8664_odbc.2.12.86.tgz',
+    'Key' => 'drivers/snowflake/snowflake_linux_x8664_odbc.2.12.87.tgz',
     'SaveAs' => './snowflake_linux_x8664_odbc.tgz'
 ]);
